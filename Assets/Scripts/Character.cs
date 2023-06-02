5283b86f772910585e6372e62008aa8507ecdedc
Luciano Donati <luciano.donati536@gmail.com>
2023-06-02 12:29:54 -0300

    Added mock character script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public void Jump()
+    {
+        
+    }
+
+    public void Move(Vector3 direction)
+    {
+        
+    }
+}
